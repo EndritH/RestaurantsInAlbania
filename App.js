e547658053a2afc3c14370235a00bf0924559a11
Endrit Hafizi <endrit.books@gmail.com>
2021-07-02 00:49:24 +0200

    SafeAreaView set for both platforms

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
 
+// console.log(StatusBar.currentHeight);
+const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Our blanc react native canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
