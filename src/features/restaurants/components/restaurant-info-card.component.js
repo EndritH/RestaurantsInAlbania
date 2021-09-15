a20c3639d3fe17f1f11c3e186c5b391e6641e852
Endrit Hafizi <endrit.books@gmail.com>
2021-09-16 00:13:10 +0200

    Spacer component

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Text, View, Image } from "react-native";
 import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
@@ -83,10 +84,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED
               </Text>
             )}
-            <View style={{ paddingLeft: 16 }} />
-            {isOpenNow && <Open xml={open} width={20} height={20} />}
-            <View style={{ paddingLeft: 16 }} />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
